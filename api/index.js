171f070fffbbe9601b3ba2e52117ee81fb108e15
jadeam0 <jdpaulse27@gmail.com>
2023-09-12 13:51:06 +0200

    update

--- api/index.js
+++ api/index.js
@@ -18,6 +18,16 @@ const errorHandeling = require("./middleware/errorHandeling");
 
 const port = process.env.PORT || 8000;
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header("Access-Control-Allow-Methods", "*");
+  res.header("Access-Control-Request-Methods", "*");
+  res.header("Access-Control-Allow-Headers", "*");
+  res.header("Access-Control-Expose-Headers", "Authorization");
+  next();
+});
+
 app.use(express.static("./static"));
 
 app.use(express.urlencoded({ extended: false }));
