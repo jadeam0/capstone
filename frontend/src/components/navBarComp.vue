f901a1cdcb21557f2361a3e403e0025cd2eb54a9
robynainsley21 <robynainsley2i@gmail.com>
2023-09-13 17:08:55 +0200

    added second navbar

--- frontend/src/components/navBarComp.vue
+++ frontend/src/components/navBarComp.vue
@@ -1,37 +1,115 @@
 <template>
-    <header class="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between border-bottom sticky-top">
-      <a href="/" class="d-flex align-items-center col-md-3 mb-2 mb-md-0 text-dark text-decoration-none">
-        <img class="bi me-2 logo" width="40" height="32" role="img" aria-label="Bootstrap" src="https://i.postimg.cc/MKr8bZpZ/Free-Sample-By-Wix.jpg">
+  <header
+    class="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between border-bottom sticky-top"
+  >
+    <a
+      href="/"
+      class="d-flex align-items-center col-md-3 mb-2 mb-md-0 text-dark text-decoration-none"
+    >
+      <img
+        class="bi me-2 logo"
+        width="40"
+        height="32"
+        role="img"
+        aria-label="Bootstrap"
+        src="https://i.postimg.cc/MKr8bZpZ/Free-Sample-By-Wix.jpg"
+      />
+    </a>
+
+    <ul class="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
+      <li>
+        <router-link to="/" class="nav-link px-2 link-secondary"
+          ><img
+            width="30"
+            height="30"
+            src="https://i.postimg.cc/RZfJdX2p/home.png"
+            alt="home"
+        /></router-link>
+      </li>
+      <li>
+        <router-link to="/menu" class="nav-link px-2 link-dark"
+          ><img
+            width="30"
+            height="30"
+            src="https://i.postimg.cc/qRrFqDHP/fast-food.png"
+            alt="menu"
+        /></router-link>
+      </li>
+      <li>
+        <router-link to="/about" class="nav-link px-2 link-dark"
+          ><img
+            width="30"
+            height="30"
+            src="https://i.postimg.cc/hG4gv0W3/guide-book.png"
+            alt="about"
+        /></router-link>
+      </li>
+      <li>
+        <router-link to="/feedback" class="nav-link px-2 link-dark"
+          ><img
+            width="30"
+            height="30"
+            src="https://i.postimg.cc/ZqNgTB0D/feedback.png"
+            alt="feedback"
+        /></router-link>
+      </li>
+      <li>
+        <router-link to="/admin" class="nav-link px-2 link-dark"
+          ><img
+            width="30"
+            height="30"
+            src="https://i.postimg.cc/k46xrK4B/admin-panel.png"
+            alt="admin"
+        /></router-link>
+      </li>
+
+      <li>
+        <router-link to="/cart" class="nav-link px-2 link-dark"
+          ><img
+            width="30"
+            height="30"
+            src="https://i.postimg.cc/k46xrK4B/admin-panel.png"
+            alt="cart"
+        /></router-link>
+      </li>
+    </ul>
+
+    <div class="col-md-3 text-end">
+      <button type="button" class="btn btn-outline-primary me-2 butt1">
+        <router-link to="/login">Log In</router-link>
+      </button>
+      <button type="button" class="btn btn-primary butt2">
+        <router-link to="/signup">Sign Up</router-link>
+      </button>
+    </div>
+
+    <div class="text-end">
+      <a
+        href="#"
+        class="d-block link-dark text-decoration-none"
+        id="dropdownUser1"
+        data-bs-toggle="dropdown"
+        aria-expanded="false"
+      >
+        <img
+          src="https://github.com/mdo.png"
+          alt="mdo"
+          width="32"
+          height="32"
+          class="rounded-circle"
+        />
       </a>
+    </div>
+  </header>
 
-      <ul class="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
-        <li><router-link to="/" class="nav-link px-2 link-secondary"><img width="30" height="30" src="https://i.postimg.cc/RZfJdX2p/home.png" alt="home"></router-link></li>
-        <li><router-link to="/menu" class="nav-link px-2 link-dark"><img width="30" height="30" src="https://i.postimg.cc/qRrFqDHP/fast-food.png" alt="menu"></router-link></li>
-        <li><router-link to="/about" class="nav-link px-2 link-dark"><img width="30" height="30" src="https://i.postimg.cc/hG4gv0W3/guide-book.png" alt="about"></router-link></li>
-        <li><router-link to="/feedback" class="nav-link px-2 link-dark"><img width="30" height="30" src="https://i.postimg.cc/ZqNgTB0D/feedback.png" alt="feedback"></router-link></li>
-        <li><router-link to="/admin" class="nav-link px-2 link-dark"><img width="30" height="30" src="https://i.postimg.cc/k46xrK4B/admin-panel.png" alt="admin"></router-link></li>
-
-                <li><router-link to="/cart" class="nav-link px-2 link-dark"><img width="30" height="30" src="https://i.postimg.cc/k46xrK4B/admin-panel.png" alt="cart"></router-link></li>
-
-      </ul>
-
-      <div class="col-md-3 text-end">
-        <button type="button" class="btn btn-outline-primary me-2 butt1">
-            <router-link to="/login">Log In</router-link>
-        </button>
-        <button type="button" class="btn btn-primary butt2">
-            <router-link to="/signup">Sign Up</router-link>
-        </button>
-      </div>
-
-      <div class="text-end">
-          <a href="#" class="d-block link-dark text-decoration-none" id="dropdownUser1" data-bs-toggle="dropdown" aria-expanded="false">
-            <img src="https://github.com/mdo.png" alt="mdo" width="32" height="32" class="rounded-circle">
-          </a>
-        </div>
-    </header>
-
-<!-- 
+  <div id="second-nav">
+    <div class="input-icons">
+      <input class="input" type="text" />
+      <!-- <i class="fa fa-search icon"></i> -->
+    </div>
+    <a href=""><img src="shopping-cart.png" alt="cart-icon" /></a>
+  </div>
+  <!-- 
 <div class="bottom_nav">
       <div class="left">
         <div class="logo"><p><span>Archie-Vable</span>Gaming</p></div>
@@ -85,7 +163,7 @@
     
     </div> -->
 
-    <!-- <div style="padding: 1rem;" class="header">
+  <!-- <div style="padding: 1rem;" class="header">
           <div class="sub1">
             <img src="https://i.postimg.cc/MKr8bZpZ/Free-Sample-By-Wix.jpg" alt="" class="logo">
           </div>
@@ -121,80 +199,107 @@
 </template>
 
 <script>
-export default {
-    
-}
+export default {};
 </script>
 
 <style scoped>
-header{
-    display: flex;
-    flex-direction: row;
-    flex-wrap: wrap;
-    background: #1e90ff;
-    padding: 5px;
-    /* padding-block-end: 10px; */
+header {
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  background: #1e90ff;
+  padding: 5px;
+  /* padding-block-end: 10px; */
 }
-ul{
-    display: flex;
-    flex-direction: row;
-    justify-content: flex-start;
-    list-style-type: none;    
+ul {
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-start;
+  list-style-type: none;
 }
 li {
-    margin-left: 2vh;
-    color: white;
-    font-size: 15px;
-    font-family: Arial, Helvetica, sans-serif;
-
+  margin-left: 2vh;
+  color: white;
+  font-size: 15px;
+  font-family: Arial, Helvetica, sans-serif;
 }
-a{
-    text-decoration: none;
-    color: white;
+a {
+  text-decoration: none;
+  color: white;
 }
-.logo{
-    height: 60px;
-    width: 60px;
-    border-radius: 50%;
-    /* margin-top: 9px; */
+.logo {
+  height: 60px;
+  width: 60px;
+  border-radius: 50%;
+  /* margin-top: 9px; */
 }
-.butt1,.butt2{
-    /* height: 35px; */
-    /* width: 110px; */
-    /* margin-top: 10px; */
-    background-color: #ff69b4;
-    color: white;
-    /* border: 3px solid; */
-    border-radius: 20px;
-    font-size: 15px;
-    font-weight: bold;
-    /* margin: .5rem; */
-
+.butt1,
+.butt2 {
+  /* height: 35px; */
+  /* width: 110px; */
+  /* margin-top: 10px; */
+  background-color: #ff69b4;
+  color: white;
+  /* border: 3px solid; */
+  border-radius: 20px;
+  font-size: 15px;
+  font-weight: bold;
+  /* margin: .5rem; */
 }
-.butt2{
-    background-color:#ffa500;
-    color: #fff; 
-    font-weight: bold;
+.butt2 {
+  background-color: #ffa500;
+  color: #fff;
+  font-weight: bold;
 }
-.butt1:hover{
-    /* background-color: ; */
-    color: #fff; 
-    font-size: 20px;
-    text-decoration: underline solid 1px#0f0;
+.butt1:hover {
+  /* background-color: ; */
+  color: #fff;
+  font-size: 20px;
+  text-decoration: underline solid 1px#0f0;
 }
-.butt2:hover{
-    /* opacity: 0.8; */
-    color: #fff; 
-    font-size: 20px;
-    text-decoration: underline solid 1px#0f0;
+.butt2:hover {
+  /* opacity: 0.8; */
+  color: #fff;
+  font-size: 20px;
+  text-decoration: underline solid 1px#0f0;
 }
-li:hover{
-    opacity: 0.9;
-    text-decoration: underline solid 2px#0f0;
-    text-underline-offset: 3px ;
+li:hover {
+  opacity: 0.9;
+  text-decoration: underline solid 2px#0f0;
+  text-underline-offset: 3px;
 }
 ul li a:active {
-    background: rgb(199, 245, 199);
-    color: #0f0;
+  background: rgb(199, 245, 199);
+  color: #0f0;
+}
+
+/* second navbar */
+.input {
+  border-radius: 1rem;
+  border: 2px solid;
+  background: #000;
+  opacity: 10%;
+  width: 15rem;
+}
+
+/* .input-icons i{
+    position: absolute;
+    top: 11rem;
+    right: 9.5rem;
+} */
+
+#second-nav img {
+  width: 1.5rem;
+}
+
+#second-nav {
+  display: flex;
+  justify-content: space-around;
+  padding: 1.5rem;
+}
+@media screen and (min-width: 640px) {
+  #second-nav img {
+    width: 2rem;
+  }
 }
 </style>
